f736e00c15dc39d28440fe793cf087a708503223
kazi riyad <kazimohiuddinriyad77@gmail.com>
2021-01-27 21:48:31 +0600

    some working login area section

---
+++ index.js
@@ -0,0 +1,5 @@
+const loginBtn = document.getElementById('login');
+loginBtn.addEventListener('click', function(){
+    const loginArea = document.getElementById('login_side');
+    loginArea.style.display = 'none';
+})
