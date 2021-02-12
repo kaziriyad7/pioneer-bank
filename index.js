98cf5d41c2449fd8e4eec97b9daf2abf18328492
kazi riyad <kazimohiuddinriyad77@gmail.com>
2021-02-13 00:13:18 +0600

    add deposit number verification

--- index.js
+++ index.js
@@ -1,48 +1,54 @@
 // Login area button
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function () {
-  const loginArea = document.getElementById("login_side");
-  loginArea.style.display = "none";
-  const transationArea = document.getElementById("transation_area");
-  transationArea.style.display = "block";
+    const loginArea = document.getElementById("login_side");
+    loginArea.style.display = "none";
+    const transationArea = document.getElementById("transation_area");
+    transationArea.style.display = "block";
 });
 
 // Deposite area button
 const DepositBtn = document.getElementById("addDeposit");
 DepositBtn.addEventListener("click", function () {
-  
+    const depositNumber = getInputNumber("depositAmount");
 
-   
-  updateSpanText('currentDeposit', depositNumber);
-  updateSpanText("currentBalance", depositNumber);
+    // add deposit number verification
+    if (depositNumber < 0) {
+        alert("Deposit amount can't be Negative or Less than 0");
+    }
+    else {
+        updateSpanText("currentDeposit", depositNumber);
+        updateSpanText("currentBalance", depositNumber);
 
-  document.getElementById("depositAmount").value = "";
+        document.getElementById("depositAmount").value = "";
+
+    }
 });
 
 // withdraw button event handler
-const withdrawBtn = document.getElementById('addwithdraw');
-withdrawBtn.addEventListener('click', function(){
-    const withdrawNumber = getInputNumber('withdrawAmount');
+const withdrawBtn = document.getElementById("addwithdraw");
+withdrawBtn.addEventListener("click", function () {
+    const withdrawNumber = getInputNumber("withdrawAmount");
 
-  updateSpanText('currentWithdraw',withdrawNumber);
-  updateSpanText("currentBalance", -1*withdrawNumber);
-   
+    // working by withdraw/withdraw button and total balance
+    updateSpanText("currentWithdraw", withdrawNumber);
+    updateSpanText("currentBalance", -1 * withdrawNumber);
 
     document.getElementById("withdrawAmount").value = "";
 
     // console.log(withdrawNumber);
-})
+});
 
-function getInputNumber(id){
+function getInputNumber(id) {
     const amount = document.getElementById(id).value;
     const amountNumber = parseFloat(amount);
     return amountNumber;
 }
 
 function updateSpanText(id, depositNumber) {
-  // current balance capture
-  const current = document.getElementById(id).innerText;
-  const currentNumber = parseFloat(current);
-  const totalAmount = depositNumber + currentNumber;
-  document.getElementById(id).innerText = totalAmount;
+    // current balance capture
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = depositNumber + currentNumber;
+    document.getElementById(id).innerText = totalAmount;
 }
